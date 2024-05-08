7d5cde0350eb13efc9adc42b4c116898f3c03e16
Elif Sak <es3951@columbia.edu>
2024-05-08 11:48:26 +0200

    final edits

--- static/practice.js
+++ static/practice.js
@@ -1,35 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   let totalTime = 240; // Total time in seconds (4 minutes)
-//   const timerElement = document.getElementById('timer');
-
-//   if (timerElement) { // Check if the timer element exists
-//     startTimer(totalTime, timerElement);
-//   } else {
-//     console.error('The timer element was not found!');
-//   }
-// });
-
-// function startTimer(duration, display) {
-//   let timer = duration,
-//     minutes,
-//     seconds;
-//   const interval = setInterval(function () {
-//     minutes = parseInt(timer / 60, 10);
-//     seconds = parseInt(timer % 60, 10);
-
-//     minutes = minutes < 10 ? '0' + minutes : minutes;
-//     seconds = seconds < 10 ? '0' + seconds : seconds;
-
-//     display.textContent = minutes + ':' + seconds;
-
-//     if (--timer < 0) {
-//       clearInterval(interval);
-//       display.textContent = 'Time is up!';
-//       // Add any additional actions here when the timer ends
-//     }
-//   }, 1000);
-// }
-
 document.addEventListener('DOMContentLoaded', function () {
   let totalTime = 240; // Total time in seconds (4 minutes)
   const timerElement = document.getElementById('timer');
@@ -63,8 +31,8 @@ function startTimer(duration, display) {
 }
 
 function timeUpWarning() {
-  // Create a modal or alert to inform the user
+  // Create an alert to inform the user
   alert('Time is up! You will now be taken to the quiz.');
   // Redirect to the quiz page
-  window.location.href = '/quiz/1'; // Adjust according to your routing setup
+  window.location.href = '/quiz/1'; 
 }
